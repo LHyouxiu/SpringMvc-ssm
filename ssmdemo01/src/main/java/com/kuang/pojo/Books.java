8fe3a95c88c9c5df31ea131770ab15d489033eb2
liuhao <liuhao@drail.com.cn>
2021-06-10 14:56:06 +0800

    ssm整合完毕

--- ssmdemo01/src/main/java/com/kuang/pojo/Books.java
+++ ssmdemo01/src/main/java/com/kuang/pojo/Books.java
@@ -3,12 +3,10 @@ package com.kuang.pojo;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@ToString
 public class Books {
 
     private int bookID;
